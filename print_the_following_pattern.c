3d6727df19007a14e984e797efdcbd7b7504ac4b
aamir1319 <108738138+aamir1319@users.noreply.github.com>
2022-10-04 15:10:08 +0530

    #include<stdio.h> int main(){     int n,i,j,k,n_k,n_j;     scanf("%d",&n);     n_k=n-1;     n_j=1;     for(i=1;i<=n;i++){         for(k=n_k;k>=i;k--){                 printf(" ");             }             for(j=0;j<n_j;j++){                 printf("%d",i);             }             n_j+=2;             printf(" ");         }         return 0; }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,13 +1,19 @@
 #include<stdio.h>
 int main(){
-    int n,i,j;
+    int n,i,j,k,n_k,n_j;
     scanf("%d",&n);
-    for(i=n;i>=1;i--){
-        for(j=n;j>=1;j--){
-            printf("%d ",j);
-        }
+    n_k=n-1;
+    n_j=1;
+    for(i=1;i<=n;i++){
+        for(k=n_k;k>=i;k--){
+                printf(" ");
+            }    
+            for(j=0;j<n_j;j++){
+                printf("%d",i);
+            }
+            n_j+=2;
             printf("
 ");
-    }
-    return 0;
-}    
+        }
+        return 0;
+}
