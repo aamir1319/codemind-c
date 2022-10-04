a737b40325fa09464382d088cd994525f6e66285
aamir1319 <108738138+aamir1319@users.noreply.github.com>
2022-10-04 15:22:15 +0530

    #include<stdio.h> int main(){     int n,i,j,last;     scanf("%d",&n);     last=65+n;     for(i=65;i<last;i++){         for(j=0;j<n;j++){             printf("%c ",i);         }         printf(" ");     }
        return 0;
    }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,20 +1,15 @@
 #include<stdio.h>
 int main(){
-    int n,i,j,k,l;
+    int n,i,j,last;
     scanf("%d",&n);
-    for(i=0;i<n;i++){
-        for(j=n-2;j>=i;j--){
-                printf(" ");
-            }    
-            for(k=i;k>=1;k--){
-                printf("%d",k);
-            }
-            for(l=0;l<=i;l++){
-                printf("%d",l);
-            }
-            printf("
+    last=65+n;
+    for(i=65;i<last;i++){
+        for(j=0;j<n;j++){
+            printf("%c ",i);
+        }    
+        printf("
 ");
-            
-        }
-        return 0;
+    }
+           
+    return 0;
 }
