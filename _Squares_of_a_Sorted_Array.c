2b885d7376032974dfddc8a848ad0505f778cdef
aamir1319 <108738138+aamir1319@users.noreply.github.com>
2022-10-16 09:35:10 +0530

    #include<stdio.h>
    int main()
    {
        int n,i,j;
        scanf("%d",&n);
        int arr[n];
        for(i=0;i<n;i++)
        {
            scanf("%d",&arr[i]);
            arr[i] = arr[i]*arr[i];
        }
        
        for(i=0;i<n;i++)
        {
            for(j=0;j<n;j++)
            {
                if (arr[i]<arr[j])
                {
                    int temp = arr[i];
                    arr[i] = arr[j];
                    arr[j] = temp;
                }
            }
        }
        
        for(i=0;i<n;i++)
        {
            printf("%d ",arr[i]);
        }
        
        return 0;
    }

---
+++ _Squares_of_a_Sorted_Array.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+int main()
+{
+    int n,i,j;
+    scanf("%d",&n);
+    int arr[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+        arr[i] = arr[i]*arr[i];
+    }
+    
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            if (arr[i]<arr[j])
+            {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+    
+    for(i=0;i<n;i++)
+    {
+        printf("%d ",arr[i]);
+    }
+    
+    return 0;
+}
